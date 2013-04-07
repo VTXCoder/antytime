48b0f668595aec52525bc64237dff0c55a66339a
Daniel Haigh <daniel.haigh@oasis.com>
2013-04-07 16:41:55 +1000

    .

--- static/javascripts/matrix.js
+++ static/javascripts/matrix.js
@@ -3,7 +3,8 @@
 var matrixObject=function() {
 
 	this.$c=null;
-	this.posSize=0;
+	//this.posSize=0;
+	this.squareSize=20;
 
 	// Matrix will be downloaded and created
 	this.create=function(name) {
@@ -11,57 +12,94 @@ var matrixObject=function() {
 		game.server.getMatrix(name,function(data) {
 			console.log("Matrix Received");
 			console.log(data);
+			game.matrix.init(data);
+			game.matrix.showGrid();
+			//game.matrix.showGridPositions();
+			//game.matrix.createSprite("ant","anty",20,20);
 		});
 	};
 
 
-	this.init=function() {
-		console.log("Initialising");
-		//this.createCanvas();
+	this.init=function(data) {
+		console.log("Initialising Matrix: "+data.name);
+		self=this;
 		this.$c=$("#canvas");
-		this.posSize=this.$c.attr("width")/50;
+		this.$c=$("<canvas />",{id:'canvas'}).attr({width:data.width*this.squareSize,height:data.height*this.squareSize});
+		this.$c.scaleCanvas(2);
+		$("#game").html("");
+		$("#game").append(this.$c);
+
+		// Draw the main rectangle
+		this.$c.drawRect({
+		  layer:true,
+		  fillStyle: "#fff",
+		  x: 0, y: 0,
+		  width: (data.width*this.squareSize),
+		  height: (data.height*this.squareSize),
+		  fromCenter: false,
+		  mousemove: function(layer) {
+		  	var dx, dy;
+    		dx = layer.eventX - layer.x;
+    		dy = layer.eventY - layer.y;
+    		posX=Math.ceil(dx/self.squareSize);
+    		posY=Math.ceil(dy/self.squareSize);
+    		
+    		console.log("pos:" +dx+"/"+dy+" ("+posX+"/"+posY+")");
+		  }
+		});
 	};
 
 
 
+	this.showGridPositions=function() {
+		//this.$c.on("mouseover",function() {
+		//	console.log("over");
+		//});
+	};
 
+	this.showGrid=function() {
+		var x,y,w=this.$c.width(), h=this.$c.height();
+
+		
+
+		for (x=this.squareSize;x < w; x=x+this.squareSize) {
+			this.$c.drawLine({
+				layer:true,
+				strokeStyle: "#ddd",
+				x1: x, 
+				y1: 0, 
+				x2: x, 
+				y2: h 
+			});
+		}
+
+		for (y=this.squareSize;y < h; y=y+this.squareSize) {
+			this.$c.drawLine({layer:true,
+				strokeStyle: "#ddd",
+				x1: 0, y1: y, x2: w, y2:y 
+			});
+		}
+
+	};
+
+
+
+	/*
 	this.createSprite=function(type,name,xPos,yPos) {
 		console.log("Creating Sprite: "+name+" ("+type+") ");
 		var s=new sprite(type,name);
 		s.setPosition(xPos,yPos);
 		return s;
 	};
+	*/
 
-	this.showGrid=function() {
-		var x,y,w=this.$c.attr("width"), h=this.$c.attr("height");
-
-		for (x=0;x < w; x=x+w/50) {
-			this.$c.drawLine({
-				layer:true,
-				strokeStyle: "#eee",
-				x1: x, y1: 0, x2: x, y2:h 
-			});
-		}
-
-		for (y=0;y < h; y=y+h/50) {
-			this.$c.drawLine({
-				layer:true,
-				strokeStyle: "#eee",
-				x1: 0, y1: y, x2: w, y2:y 
-			});
-		}
-	};
-
-	this.createCanvas=function() {
-
-
-	};
+	_.bindAll();
 
 };
 
 
 
-
+/*
 var sprite=function(type,name) {
 	this.x=null;
 	this.y=null;
@@ -146,3 +184,4 @@ var sprite=function(type,name) {
 	sprites.push(this);
 };
 
+*/
